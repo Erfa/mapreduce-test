b9f5e7721e0ccd4e9f71f719431a802c1049880e
Erik Fahlén <erik@feomedia.com>
2016-10-05 10:43:04 +0200

    Don't import mapreduce from appengine, and use generators.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
-from google.appengine.ext.mapreduce.mapreduce_pipeline import MapreducePipeline
+from google.appengine.ext import ndb
+from mapreduce import base_handler
+from mapreduce.mapreduce_pipeline import MapreducePipeline
+from mapreduce.output_writers import OutputWriter
 
+
+class User(ndb.Model):
+    name = ndb.StringProperty()
 
 app = Flask(__name__)
 
@@ -11,16 +17,28 @@ def appengine_mapper(e):
 def appengine_reduce(e):
     pass
 
+class Pipeline(base_handler.PipelineBase):
+    def run(self):
+        output = yield MapreducePipeline(
+            "index",
+            "main.index_map",
+            "main.index_reduce",
+            "mapreduce.input_readers.DatastoreInputReader",
+            params={
+                "input_reader": {
+                    "entity_kind": "app.User",
+                }
+            },
+            shards=1
+        )
+
+        yield Output(output)
+
+class Output(OutputWriter):
+    def run(self, output):
+        pass
+
 @app.route('/')
 def hello_world():
-    pipeline = MapreducePipeline(
-        '',
-        'app.appengine_mapper',
-        'app.appengine_reduce',
-        'mapreduce.input_readers.DatastoreInputReader',
-        mapper_params={
-            'entity_kind': 'SomeEntity',
-        },
-    )
-
+    pipeline = Pipeline()
     pipeline.start()
